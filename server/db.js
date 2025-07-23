912399eb386601400297202bf8926670d8208cd5
MarcoDev <marcobarzoladev@gmail.com>
2025-07-23 18:45:17 -0300

    modificando el db.js para que funcione la base de datos

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
-// ⚠️ Cambiá estos valores según tu configuración local
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '44994334marco', // poné tu contraseña si tenés
-  database: 'mielissimo'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
